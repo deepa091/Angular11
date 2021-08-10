7450bea7751e2785dce234676d694980e790fe3a
deepa.saxena <deepa.saxena@cgi.com>
2021-08-10 13:14:31 +0530

    First example file

---
+++ 1.NewClass.js
@@ -0,0 +1,13 @@
+debugger;
+
+class Employee {
+    constructor(){
+        this.empName = "";
+        this.empAge = 0;
+        this.empDesignation = "Intern";
+    }
+}
+
+var employeeOne = new Employee("Deepa",30,"Developer");
+
+var employeeTwo = new Employee(Khushboo,33,"Manager");
